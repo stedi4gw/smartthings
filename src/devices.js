5fccd5a223c1611525590f34ad096a5014a4745e
Steven Wilson <host@stedi4gw.com>
2022-08-09 21:43:14 -0400

    fix: add typings

--- src/devices.js
+++ src/devices.js
@@ -4,8 +4,15 @@ import { recurseNext } from './lib/utilis'
 export const devices = () => {
   const devicesApi = `${config.smartThingsUrl}/devices`
 
+
   return {
-    /** Retrieves the full list of devices */
+    /**
+     * @typedef {Object} Device - creates a new type named 'SpecialType'
+     * @property {string} id - a string property of SpecialType
+     *//**
+    * Retrieves full list of devices
+    * @returns {Device[]}
+    */
     getDeviceList: async () => await recurseNext(devicesApi),
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { devices } from './devices'
 import { locks } from './locks'
 import { thermostats } from './thermostats'
 
+/**
+ * Access controllers for Samsung SmartThings
+ * @param {string} api_key 
+ */
 export const smartthings = (api_key) => {
   config.smartThingsKey = api_key
 
